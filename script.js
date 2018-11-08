86f53a5681485bd8a74f2d51e1bd301c63d05cfb
Jesse Ruth <jesse.ruth@oracle.com>
2018-11-08 09:07:09 -0800

    add onsubmit

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+function processForm(event) {
+  console.log('Form', event);
+}
+
 (function() {
   // your page initialization code here
   // the DOM will be available here
   console.log("Page Ready");
+
+  
 
 })();
