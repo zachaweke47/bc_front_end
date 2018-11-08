3c99756de479615141afad804a18d3ed12abe35e
Jesse Ruth <jesse.ruth@oracle.com>
2018-11-08 09:38:47 -0800

    updated request

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 function processForm(event) {
   console.log('Form', event);
   // Find our form in the DOM using its class name.
-  const form = document.getElementByClassName('#sample-form')[0];
+  const form = document.forms[0];
+  const formData = toJSONString(form)
+  console.log(formData);
+  // construct an HTTP request
+  var xhr = new XMLHttpRequest();
+  xhr.open(form.method, form.action, true);
+  xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
 
-// Get the form data with our (yet to be defined) function.
-  const data = getFormDataAsJSON(form);
-  console.log(data);
+  // send the collected data as JSON
+  xhr.send(formData);
+
+  xhr.onloadend = function () {
+    // done
+    console.log('Did It');
+  };
 }
 
-(function() {
+function toJSONString(form) {
+  var obj = {};
+  var elements = form.querySelectorAll("input, select, textarea");
+  for (var i = 0; i < elements.length; ++i) {
+    var element = elements[i];
+    var name = element.name;
+    var value = element.value;
+
+    if (name) {
+      obj[name] = value;
+    }
+  }
+
+  return JSON.stringify(obj);
+}
+
+window.onload = function pageLoaded() {
   // your page initialization code here
   // the DOM will be available here
   console.log("Page Ready");
 
-
-
-})();
+  document.getElementById('sample-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(toJSONString(e.target));
+  });
+};
